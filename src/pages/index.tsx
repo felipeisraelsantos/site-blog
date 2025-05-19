6e693f5950e38157cfe65775b2e650e5a7f42b5f
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2025-05-18 22:13:37 -0300

    feat: construindo layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { Button } from "@/components/ui/button";
-import { Header } from "@/components/header";
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -17,7 +15,6 @@ export default function Home() {
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <Header />
     </div>
   );
 }
