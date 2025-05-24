81e6ea6ff51890bd61e2cf8b6dde82a2fcbb0646
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2025-05-24 19:33:25 -0300

    feat: inserindo imagem de background

--- src/components/call-to-action/call-to-action.tsx
+++ src/components/call-to-action/call-to-action.tsx
@@ -4,16 +4,14 @@ import { Button } from "../ui/button";
 
 export const CallToAction = () => {
   return (
-    <section
-      className="py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700"
-      style={{ backgroundImage: `url(/background-call-to-action.svg)` }}
-    >
-      <div className="container">
+    <section className="relative py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+      <div className="absolute inset-0 bg-[url('/background-call-to-action.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
+      <div className="container relative ">
         <div className="flex flex-col items-center gap-6 text-center">
           <div className="p-4 bg-cyan-300 w-fit rounded-full">
             <Store className="text-cyan-100" />
           </div>
-           <h2 className="font-sans text-gray-100 text-balance text-heading-xl">
+          <h2 className="font-sans text-gray-100 text-balance text-heading-xl">
             Crie uma loja online e inicie suas vendas ainda hoje
           </h2>
           <Button variant="primary" asChild className="mt-6">
