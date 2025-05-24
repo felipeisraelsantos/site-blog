ea494f4c3fcaa6e0e8606cb9708e3a004a626a9d
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2025-05-24 14:51:18 -0300

    feat: criando footer  e header e componetizando o logo

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,17 +1,13 @@
-import Image from "next/image";
 import Link from "next/link";
+import { Logo } from "../ui/logo";
 
 export const Footer = () => {
   return (
     <footer className="border-t border-white/10">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between md:flex-row gap-8 py-8">
-          <Link href="/">
-            <Image src="/logo.svg" alt="Logo site" width={116} height={32} />
-          </Link>
-          <nav
-            className="flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foreground">
-
+          <Logo />
+          <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foreground">
             <Link href="/termos-de-uso" className="hover:text-primary">
               Termos de uso
             </Link>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,8 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { Button } from '../ui/button';
-import { ActiveLink } from '../active-link/active-link';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Button } from "../ui/button";
+import { ActiveLink } from "../active-link/active-link";
+import { Logo } from "../ui/logo";
 
 export const Header = () => {
   const router = useRouter();
@@ -10,15 +11,12 @@ export const Header = () => {
     <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 ls:px-80">
         <div className="flex h-16 items-center justify-between">
-          <Link href="/">Logo</Link>
-
+            <Logo/>
           <nav className="flex items-center gap-6">
-            <ActiveLink href="/" >Início</ActiveLink>
-            <ActiveLink href="/blog" >Blog</ActiveLink>
+            <ActiveLink href="/">Início</ActiveLink>
+            <ActiveLink href="/blog">Blog</ActiveLink>
             <Button variant="secondary" asChild>
-                <Link href="comecar">
-                    Começar
-                </Link>
+              <Link href="comecar">Começar</Link>
             </Button>
           </nav>
         </div>
