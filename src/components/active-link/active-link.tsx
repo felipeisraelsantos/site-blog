8b5ab0594490edbcc549038bc6f7ffb82105afe8
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2025-05-27 21:41:10 -0300

    feat: criando pagina de blog

--- src/components/active-link/active-link.tsx
+++ src/components/active-link/active-link.tsx
@@ -14,7 +14,7 @@ export const ActiveLink = ({ children, href, ...rest }: ActiveLinkProps) => {
 
   return (
     <Link
-      href="/"
+      href={href}
       className={cn(
         "text-action-sm transition-colors hover:text-blue-200",
         isCurrentPah ? "text-blue-200" : "text-gray-100"
